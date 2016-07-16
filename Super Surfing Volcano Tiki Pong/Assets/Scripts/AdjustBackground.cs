5379b57a621db089b0361a2e7aa8066f0c7fe158
theJeeves <larad@uci.edu>
2016-07-16 15:56:54 -0700

    07/16/16 3:56PM
    +PixelPerfectCam
    +Self Adjusting Background Script
    + Created Animations for Surfer Girl

---
+++ Super Surfing Volcano Tiki Pong/Assets/Scripts/AdjustBackground.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustBackground : MonoBehaviour {
+
+    [SerializeField]
+    private Vector2 bgSize = new Vector2(480, 360);
+    [SerializeField]
+    private bool scaleW = true;
+    [SerializeField]
+    private bool scaleH = true;
+
+	// Use this for initialization
+	void Start ()
+    {
+        float widthAdjustment = !scaleW ? 1 : Screen.width / (bgSize.x * PixelPerfectCam.scale);
+        float heightAdjustment = !scaleH ? 1 : Screen.height / (bgSize.y * PixelPerfectCam.scale);
+
+        transform.localScale = new Vector3(Mathf.CeilToInt(bgSize.x * widthAdjustment),
+            Mathf.CeilToInt(bgSize.y * heightAdjustment), 1);
+	}
+}
---
+++ Super Surfing Volcano Tiki Pong/Assets/Scripts/PixelPerfectCam.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfectCam : MonoBehaviour {
+
+    public static float pixelsToUnits = 1.0f;
+    public static float scale = 1.0f;
+    public Vector2 nativeResolution = new Vector2(1280, 720);
+
+	void Awake()
+    {
+        Camera camera = GetComponent<Camera>();
+
+        if (camera.orthographic)
+        {
+            scale = Screen.height / nativeResolution.y;
+            pixelsToUnits *= scale;
+            camera.orthographicSize = (Screen.height / 2.0f) / pixelsToUnits;
+        }
+    }
+}
