5fa3d2d17d514d0df0e697334ed1162ee6574892
theJeeves <larad@uci.edu>
2016-07-22 02:35:55 -0700

    07/22/16 2:34AM
    + Added Fireball collision detection script
    + Added Fireball movement: fireball's angle changes appropriately as it
    bounces off the walls and changes directions

---
+++ Super Surfing Volcano Tiki Pong/Assets/Scripts/Collisions/FBCollisionEvent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FBCollisionEvent : MonoBehaviour {
+
+    public delegate void FBCollisionAction(GameObject collisionObject);
+
+    public static event FBCollisionAction onEnvironmentCollision;
+    public static event FBCollisionAction onOceanCollision;
+    public static event FBCollisionAction onDamagePlayerCollision;
+    public static event FBCollisionAction onTikiCollision;
+
+    void OnCollisionEnter2D(Collision2D collider) {
+
+        string collisionObject = collider.gameObject.tag;
+
+        if (collisionObject == "Sky" || collisionObject == "Ocean") {
+            if (onEnvironmentCollision != null) {
+                onEnvironmentCollision(collider.gameObject);
+            }
+        }
+        else if (collisionObject == "DamagePlayer") {
+            if (onDamagePlayerCollision != null) {
+                onDamagePlayerCollision(collider.gameObject);
+            }
+        }
+        else if (collisionObject == "Tiki") {
+            if (onTikiCollision != null) {
+                onTikiCollision(collider.gameObject);
+            }
+        }
+    }
+}
---
+++ Super Surfing Volcano Tiki Pong/Assets/Scripts/FBMovement.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FBMovement : MonoBehaviour {
+
+    public static float fbSpeed = 100.0f;
+
+    [SerializeField]
+    private float horiztonalDirection = -1.0f;
+    [SerializeField]
+    private float verticalDirection = 1.0f;
+
+    private Rigidbody2D body2D;
+    private float tan;
+    private float angle;
+
+	// Use this for initialization
+	void Start () {
+
+        body2D = GetComponent<Rigidbody2D>();
+        verticalDirection = Random.Range(-1.0f, 1.0f);
+        CalculateTan();
+
+        angle = Mathf.Rad2Deg * Mathf.Atan(tan);
+
+        if (angle > 45) {
+            angle = 90 - angle;
+        }
+        else if (angle < -45) {
+            angle = 90 + angle;
+        }
+
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        body2D.velocity = new Vector2(fbSpeed * horiztonalDirection, fbSpeed * verticalDirection);
+    }
+
+    void OnEnable() {
+        FBCollisionEvent.onEnvironmentCollision += FBHitEnvironment;
+        FBCollisionEvent.onDamagePlayerCollision += FBHitDamagePlayer;
+        FBCollisionEvent.onTikiCollision += FBHitTiki;
+    }
+
+    void OnDisable() {
+        FBCollisionEvent.onEnvironmentCollision -= FBHitEnvironment;
+        FBCollisionEvent.onDamagePlayerCollision -= FBHitDamagePlayer;
+        FBCollisionEvent.onTikiCollision -= FBHitTiki;
+    }
+
+    private void FBHitEnvironment(GameObject sky) {
+
+        CalculateTan();
+        verticalDirection *= -1.0f;
+        ChangeRotation();
+    }
+
+    private void FBHitDamagePlayer(GameObject playerDamage) {
+
+        CalculateTan();
+        horiztonalDirection *= -1.0f;
+        ChangeFacingDirection();
+        ChangeRotation();
+
+    }
+
+    private void FBHitTiki(GameObject tiki) {
+
+        CalculateTan();
+        horiztonalDirection *= -1.0f;
+        ChangeFacingDirection();
+        ChangeRotation();
+    }
+
+    private void CalculateTan() {
+
+        if (verticalDirection > 0) {
+            tan = verticalDirection / horiztonalDirection;
+        }
+        else if (verticalDirection < 0) {
+            tan = horiztonalDirection / verticalDirection;
+        }
+    }
+
+    private void ChangeFacingDirection() {
+        transform.localScale = new Vector3(-horiztonalDirection, 1, 1);
+    }
+
+    private void ChangeRotation() {
+
+        angle = -1 * Mathf.Rad2Deg * Mathf.Atan(tan);
+
+        if (angle > 45) {
+            angle = 90 - angle > 0 ? 90 - angle : angle;
+        }
+        else if (angle < -45) {
+            angle = -1 * (90 + angle);
+        }
+
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+}
